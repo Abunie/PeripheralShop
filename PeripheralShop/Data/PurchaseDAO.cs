7fda2591b4f7dcf5248808db387a3096fa28d7ca
Abuni Amanda Gaiya <32043872+Abunie@users.noreply.github.com>
2021-05-13 00:00:36 -0600

    Update PurchaseDAO

--- PeripheralShop/Data/PurchaseDAO.cs
+++ PeripheralShop/Data/PurchaseDAO.cs
@@ -9,7 +9,7 @@ namespace PeripheralShop.Data
 {
     internal class PurchaseDAO
     {
-        private string connectionString = @"Server=tcp:abuni466.database.windows.net,1433;Initial Catalog=PeripheralShop;Persist Security Info=False;User ID=abuni;Password=Comput466@2021;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private string connectionString = @"Server=tcp:abuni466.database.windows.net,1433;Initial Catalog=PeripheralShop;Persist Security Info=False;User ID=abuni;Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         //performs all operations on the database. get all, create, delete, get one, search etc.
         public void Insert(String UserId, String Desc)
         {
